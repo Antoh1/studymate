6c4b1472eea896e2af1caa3318256474313f29ad
Antoh1 <antotiro@gmail.com>
2023-05-03 23:45:14 +0300

    [UPDT] link users/hosts, topic, messages and rooms

--- base/views.py
+++ base/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Room
 
-rooms = [
-    {'id':1, 'name':'The art of python'},
-    {'id':2, 'name':'Jungle of Django'},
-    {'id':3, 'name':'The AI Revolution'},
-]
+# rooms = [
+#     {'id':1, 'name':'The art of python'},
+#     {'id':2, 'name':'Jungle of Django'},
+#     {'id':3, 'name':'The AI Revolution'},
+# ]
 
 def home(request):
     rooms = Room.objects.all()
