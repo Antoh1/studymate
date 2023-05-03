4a78172091fc27162501c6e1618c64e5eb574848
Antoh1 <antotiro@gmail.com>
2023-05-03 14:17:20 +0300

    UPDT templates restructure and rendering

--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id':1, 'name':'The art of python'},
+    {'id':2, 'name':'Jungle of Django'},
+    {'id':3, 'name':'The AI Revolution'},
+]
 
 def home(request):
-    return render(request, 'index.html')
+    context = {'rooms':rooms}
+    return render(request, 'base/index.html', context)
 
 def room(request):
-    return render(request, 'room.html')  
+    return render(request, 'base/room.html')  
