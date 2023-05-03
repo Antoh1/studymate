c8ea24c5f2742fb723ffc92289b2c42688dea4ba
Antoh1 <antotiro@gmail.com>
2023-05-03 22:34:08 +0300

    [ADD added model class]

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True) #blank not to submit empty form, null>db
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
