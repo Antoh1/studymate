91c4444cd3faf6a2153ec8a5e65b038a99fe624f
Antoh1 <antotiro@gmail.com>
2023-05-06 17:06:09 +0300

    [UPDT] user management functionality(login,register, logout page restrictions)

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='study-home'),
+    path('login/', views.loginPage, name='study-login'),
+    path('logout/', views.logoutUser, name='logout-user'),
+    path('rigister/', views.registerPage, name='study-register'),
     path('room/<str:pk>/', views.room, name='study-room'),
     path('create-form', views.create_room, name='room-form'),
     path('update-form/<str:pk>/', views.update_room, name='update-form'),
